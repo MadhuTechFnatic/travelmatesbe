c2f448ad0d2f7837203683052d7be57b89065889
[YOUR_GITHUB_USERNAME] <bagammagarimadhu@gmail.com>
2024-03-09 12:10:27 +0530

    minor changes in reqriments

--- travelmatesb/settings.py
+++ travelmatesb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1h7i9q1-h^35ni!1m7_2a*=md_xa^9yw%xfm(qu-#@ghw-7a7t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
